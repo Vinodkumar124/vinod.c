f1aad6846afe82696a593ea0315e33fd7ffdefc3
Vinodkumar124 <111114386+Vinodkumar124@users.noreply.github.com>
2022-08-23 00:51:56 -0700

    assignment 7.c

---
+++ employeid.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int hours,id;
+float salary=15000,sal;
+printf("The employee id= ");
+scanf("%d",&id);
+printf("working hours = ");
+scanf("%d",&hours);
+printf(" per hours salary = ");
+scanf("%f",&salary);
+printf("salary =u$%.2f",salary*hours);
+	
+	
+}
